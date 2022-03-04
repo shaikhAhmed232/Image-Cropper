dbe0b1d6bd622081144420f112a9e8cefac4aeb7
ShaikhAhmed232 <stitans270@gmail.com>
2022-03-04 21:32:26 +0530

    this is image cropper

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useRef } from "react";
+import Cropper from "react-easy-crop";
+import { generateDownload } from "./utils/cropImages";
 
-function App() {
+const App = () => {
+  const inputRef = useRef();
+  const [src, setSrc] = useState(null);
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [croppedArea, setCroppedArea] = useState(null);
+  const [zoom, setZoom] = useState(1);
+
+  const fileChangeHandle = (event) => {
+    const { files } = event.target;
+    setSrc(URL.createObjectURL(files[0]));
+  };
+
+  const onCropCompleted = (cropPercentage, cropPixels) => {
+    setCroppedArea(cropPixels);
+  };
+
+  const onDownloaded = () => {
+    generateDownload(src, croppedArea);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="input-container">
+        <input
+          type="file"
+          accept="image/*"
+          ref={inputRef}
+          onChange={fileChangeHandle}
+        />
+        <button onClick={() => inputRef.current.click()}>Upload</button>
+      </div>
+      <div className="image-container">
+        {src && (
+          <>
+            <div className="cropper-container">
+              <Cropper
+                image={src}
+                crop={crop}
+                zoom={zoom}
+                aspect={16 / 9}
+                onCropChange={(c) => setCrop(c)}
+                onZoomChange={(z) => setZoom(z)}
+                onCropComplete={onCropCompleted}
+              />
+            </div>
+            <div className="slider">
+              <input
+                type="range"
+                min={1}
+                max={3}
+                step={0.1}
+                value={zoom}
+                onChange={(e) => setZoom(e.target.value)}
+              />
+            </div>
+            <button className="download-btn" onClick={onDownloaded}>
+              Download
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
